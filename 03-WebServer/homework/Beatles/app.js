a04c91b255dfc6065a5a91d6ac4d189715d0e23f
juaniarrigoni <juani.arrigoni@gmail.comgit config --global user.name juaniarrigonigit initgit config --global user.email juani.arrigoni@gmail.com>
2022-06-29 13:06:35 -0300

    hw3 m3

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -35,6 +35,7 @@ http
       res.writeHead(200, { "Content-Type": "application/json" });
       return res.end(JSON.stringify(beatles));
     }
+    //
     if (req.url.substring(0, 5) === "/api/") {
       const beatle = req.url.split("/").pop();
       const found = beatles.find((b) => encodeURI(b.name) === beatle);
@@ -46,6 +47,37 @@ http
         return res.end(`${decodeURI(beatle)} no existe`);
       }
     }
+    if (req.url == "/") {
+      fs.readFile("./index.html", function (err, data) {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/pain" });
+          return res.end("lo siuento bro");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        return res.end(data);
+      });
+    }
+    if (req.url.length > 1) {
+      const beatle = req.url.split("/").pop();
+      const found = beatles.find((b) => encodeURI(b.name) === beatle);
+      if (found) {
+        fs.readFile("./beatle.html", " utf8", function (err, data) {
+          if (err) {
+            res.writeHead(404, { "Content-Type": "text/pain" });
+            return res.end("lo siuento bro");
+          } else {
+            data = data.replace("{name}", found.name);
+            data = data.replace("{birthdate}", found.birthdate);
+            data = data.replace("{profilePic}", found.profilePic);
+            //
+            res.writeHead(200, { "Content-Type": "text/html" });
+            return res.end(data);
+          }
+        });
+      } else {
+        res.writeHead(404, { "Content-Type": "text/pain" });
+        return res.end("lo siuento bro");
+      }
+    }
   })
   .listen(3000, "127.0.0.1");
-console.log("todo gud".green);
